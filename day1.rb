659e9f68d429b3e01ac0526e792d896aa849abcb
Adam Lukens <adam@testdouble.com>
2020-12-04 02:14:06 -0500

    Day 1 second star

--- day1.rb
+++ day1.rb
@@ -14,6 +14,25 @@ class Day1
 
     first * second
   end
+
+  def self.find_2020_triple(input)
+    (0 ... input.length).each do |i|
+      (i+1 ... input.length).each do |j|
+        (j+1 ... input.length).each do |k|
+          first, second, third = input[i], input[j], input[k]
+          return [first, second, third] if first + second + third == 2020
+        end
+      end
+    end
+  end
+
+  def self.solve_triple(input)
+    input = input.map(&:to_i)
+    first, second, third = find_2020_triple(input)
+
+    first * second * third
+  end
 end
 
-puts Day1.solve_pair(ARGV) if __FILE__ == $0
+puts "Product of two entries: #{Day1.solve_pair(ARGV)}" if __FILE__ == $0
+puts "Product of three entries: #{Day1.solve_triple(ARGV)}" if __FILE__ == $0
--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -19,6 +19,12 @@ RSpec.describe Day1 do
     end
   end
 
+  describe 'find_2020_triple' do
+    it 'finds the three numbers that add up to 2020' do
+      expect(Day1.find_2020_triple(input)).to eq [979, 366, 675]
+    end
+  end
+
   describe 'solve_pair' do
     it 'finds two entries that sum to 2020 and multiplies them' do
       expect(Day1.solve_pair(input)).to eq 514579
