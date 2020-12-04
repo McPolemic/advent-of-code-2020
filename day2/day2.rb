89956e1f7b6dfde8fd07214cbed039790df8b9d0
Adam Lukens <adam@testdouble.com>
2020-12-04 02:33:20 -0500

    Day 2 second star

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,4 +1,4 @@
-class PasswordParser
+class LegacyPasswordParser
   attr_reader :min, :max, :letter, :password
 
   def initialize(min, max, letter, password)
@@ -25,6 +25,38 @@ class PasswordParser
     false
   end
 end
+
+class PasswordParser
+  attr_reader :first_index, :second_index, :letter, :password
+
+  def initialize(first_index, second_index, letter, password)
+    # Convert the indices from 1-indexed to 0-indexed on .new
+    @first_index = first_index.to_i - 1
+    @second_index = second_index.to_i - 1
+    @letter = letter
+    @password = password
+  end
+
+  def self.parse_from_line(line)
+    rule, password = line.split(": ")
+    # Pull apart the string "1-3 a"
+    # 1 - first character to look at (1-indexed)
+    # 3 - second character to look at (1-indexed)
+    # a - the letter
+    _, first_index, second_index, letter = rule.match(/(\d+)\-(\d+) (\w)/).to_a
+    self.new(first_index, second_index, letter, password)
+  end
+
+  def valid_password?
+    count = [
+      password[first_index],
+      password[second_index]
+    ].count(letter)
+
+    return count == 1
+  end
+end
+
 
 class Day2
   def self.password_valid?(line)
--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -2,26 +2,51 @@ require 'spec_helper'
 require_relative '../day2/day2'
 require 'stringio'
 
-RSpec.describe Day2 do
-  describe '.password_valid?' do
+RSpec.describe LegacyPasswordParser do
+  describe '.valid_password?' do
     # This example contain 1-3 "a"s (one "a", to be precise)
-    xit 'says that a password that follows the rules is valid' do
+    it 'says that a password that follows the rules is valid' do
       input = "1-3 a: abcde"
-      expect(Day2.password_valid?(input)).to be true
+      result = LegacyPasswordParser.parse_from_line(input).valid_password?
+
+      expect(result).to be true
     end
 
     it 'says that a password that does not follow the rules is invalid' do
       # This example does not contain 1-3 "b"s
       input = "1-3 b: cdefg"
-      expect(Day2.password_valid?(input)).to be false
+      result = LegacyPasswordParser.parse_from_line(input).valid_password?
+      expect(result).to be false
 
       # This example has multiple digits in the min/max and not enough "k"s
       input = "19-20 k: kkkkkkkkkkkkkkdkkkkx"
-      expect(Day2.password_valid?(input)).to be false
+      result = LegacyPasswordParser.parse_from_line(input).valid_password?
+      expect(result).to be false
     end
   end
+end
 
-  xdescribe 'solve' do
+RSpec.describe PasswordParser do
+  describe '.valid_password?' do
+    # This example contains one "a" at index 1
+    it 'says that a password that follows the rules is valid' do
+      input = "1-3 a: abcde"
+      result = PasswordParser.parse_from_line(input).valid_password?
+
+      expect(result).to be true
+    end
+
+    it 'says that a password that does not follow the rules is invalid' do
+      # This example does not contain a "b" for the first or third character
+      input = "1-3 b: cbebb"
+      result = PasswordParser.parse_from_line(input).valid_password?
+      expect(result).to be false
+    end
+  end
+end
+
+RSpec.describe Day2 do
+  describe 'solve' do
     it "accepts multiple lines and counts how many are valid" do
       input = <<~EOF
         1-3 a: abcde
